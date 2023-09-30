f81227078fd6a6f3fc5f8cf8426c0ebb919e7539
Fran pacho <pacho_luro@hotmail.com>
2023-09-30 04:54:52 -0300

    a

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <Container>
+      <h2>Sobre Nosotros</h2>
+      <img src="../Img/gatito.jpg" alt="gatito" />
+      </Container>
+      
+    </div>
   )
 }
 
